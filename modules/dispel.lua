ab1647c0a7fa03bba9b408e27421076d978a0705
Adrian L Lange <adrianlund@gmail.com>
2010-06-10 20:40:35 +0200

    Too many versions of the spell, use the name instead

--- modules/dispel.lua
+++ modules/dispel.lua
@@ -7,15 +7,16 @@ local dispel = {
 }
 
 local blacklist = {
-	[70311] = true, -- Mutated Abo @ Putricide
+	[GetSpellInfo(70311)] = true, -- Mutated Abo @ Putricide
 }
 
 local function UNIT_AURA(self, event, unit)
 	if(self.unit ~= unit) then return end
 
 	for index = 1, 40 do
-		local exists, _, _, _, type, _, _, _, _, _, spell = UnitAura(unit, index, 'HARMFUL')
-		if(exists and dispel[type] and not blacklist[spell]) then
+		local name, _, _, _, type = UnitAura(unit, index, 'HARMFUL')
+
+		if(name and dispel[type] and not blacklist[name]) then
 			local color = DebuffTypeColor[type]
 			return self:SetBackdropBorderColor(color.r, color.g, color.b)
 		end
@@ -25,7 +26,7 @@ local function UNIT_AURA(self, event, unit)
 end
 
 function module.Dispel(self)
-	self:SetAttribute('shift-type1', 'spell')
+	self:SetAttribute('type3', 'spell')
 	self:SetAttribute('spell', GetSpellInfo(51886))
 	self:RegisterEvent('UNIT_AURA', UNIT_AURA)
 end
