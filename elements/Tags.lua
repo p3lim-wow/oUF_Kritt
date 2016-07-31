ea74adf78fdeda8d19ede44b13e9b334ae81a654
Adrian L Lange <mail@p3lim.net>
2016-07-31 08:08:27 +0200

    Improve health tag logic

--- elements/Tags.lua
+++ elements/Tags.lua
@@ -5,18 +5,22 @@ local events = {
 	health = 'UNIT_HEALTH UNIT_HEALTH_FREQUENT UNIT_MAXHEALTH UNIT_CONNECTION'
 }
 
+local format = string.format
+
 for tag, method in next, {
 	leader = function(unit)
 		return UnitIsGroupLeader(unit) and '|cffffff00!|r'
 	end,
 	health = function(unit)
-		local min, max = UnitHealth(unit), UnitHealthMax(unit)
 		if(UnitIsDeadOrGhost(unit)) then
 			return '|cffff0000X|r'
 		elseif(not UnitIsConnected(unit)) then
 			return '|cff333333#|r'
-		elseif(min / max < 0.8) then
-			return ('|cffff8080%.1f|r'):format((max - min) / 1e3)
+		else
+			local cur, max = UnitHealth(unit), UnitHealthMax(unit)
+			if(cur / max < 0.8) then
+				return format('|cffff8080%.1f|r', (max - cur) / 1e3)
+			end
 		end
 	end,
 } do
